79c35a4b2cd10f3b2d0ef5eddfdbe76bcb368fa2
pksh-23514 <prabhat23514k@gmail.com>
2022-12-24 23:38:18 +0530

    Non-Blocking Parent with Child executing completed successfully

--- 6_Zombie_Non-Block_Parent/main.c
+++ 6_Zombie_Non-Block_Parent/main.c
@@ -1,7 +1,12 @@
 /*
 Name         : Prabhat Kiran
-Date         :
-Description  :
+Date         : 22nd December 2022
+Description  : WAP to avoid Child becoming Zombie without Blocking Parent.
+1. Create a Child Process and avoid it to become a Zombie.
+2. To avoid Zombie we need to call wait() but this blocks the Parent until Child terminates.
+3. So we need to use waitpid() with proper Arguments.
+4. When Child is working Parent has to continuously print some messages.
+5. When ever Child terminates, Parent has to print Child terminated and print Exit status of Child Process.
 */
 
 #include <stdio.h>
@@ -14,26 +19,26 @@ Description  :
 
 int main ()
 {
-	pid_t pid = fork ();
-	int status;
+	pid_t pid = fork ();										//To create the Child Process.
+	int status;													//To store the Child's Exit status.
 
-	if (pid > 0)
+	if (pid > 0)												//If the 'pid' is greater than 0; it is the Parent Process.
 	{
-		printf ("A Child is created with the PID %d.\n", pid);
+		printf ("A Child is created with the PID %d.\n", pid);	//To display the Child's PID.
 
-		while ((waitpid (pid, &status, WNOHANG)) == 0)
+		while ((waitpid (pid, &status, WNOHANG)) == 0)			//To wait for the Child Process to terminate without blocking the Parent Process.
 		{
 			sleep (1);
-			printf ("Parent running...\n");
+			printf ("Parent running...\n");						//To display the message continuously while waiting for the Child to terminate.
 		}
 
 		printf ("Child %d terminated normally with Exit status %d. Parent Terminating.\n", pid, status);
 	}
-	else if (pid == 0)
+	else if (pid == 0)											//If the 'pid' is 0; it is the Child Process.
 	{
 		sleep (5);
 	}
-	else
+	else														//Error Handling.
 	{
 		printf ("The Child is not created.\n");
 	}
