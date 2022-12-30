5a132130ebbc7535bd354084ab91155a9865f196
pksh-23514 <prabhat23514k@gmail.com>
2022-12-30 11:28:59 +0530

    Shared Memory implemented Successfully

--- 12_IPC_SHM/Process-1.c
+++ 12_IPC_SHM/Process-1.c
@@ -1,9 +1,19 @@
 /*
 Name         : Prabhat Kiran
-Date         :
-Description  :
-Sample Input :
-Sample Output:
+Date         : 23rd December 2022
+Description  : Implement Communication between TWO different Processes using Shared Memory (SHM).
+1. Create 2 different Process by executing 2 Different a.out (executable) Files.
+2. Create SHM from any one of the Process; either Process-1 or Process-2.
+3. In Process-1:
+a) Prompt the user to enter a String.
+b) Convert the String into Upper case.
+c) Write the Upper case String in Shared Memory.
+4. In Process-2:
+a) Read the Data from the Shared Memory.
+b) Reverse the Input which is read from SHM.
+c) Write the Reverse String in Shared Memory.
+5. Read the Data from Shared memory in Process-1.
+6. Print the Output string on 'stdout'.
 */
 
 #include <stdio.h>
@@ -16,15 +26,15 @@ Sample Output:
 
 int main ()
 {
-	int shm_id = shmget ('P', 100, IPC_CREAT | 0664);
-	if (shm_id == -1)
+	int shm_id = shmget ('P', 100, IPC_CREAT | 0664);		//To Create a Shared Memory segment with the given File permission.
+	if (shm_id == -1)										//Error Handling.
 	{
 		perror ("shmget");
 		return 0;
 	}
 
-	char* shm = (char*) shmat (shm_id, NULL, 0);
-	if (shm == (char*) (-1))
+	char* shm = (char*) shmat (shm_id, NULL, 0);			//To Attach the Process-1 with the Shared Memory.
+	if (shm == (char*) (-1))								//Error Handling.
 	{
 		perror ("shmat");
 		return 0;
@@ -35,7 +45,7 @@ int main ()
 	printf ("Writing to the Shared Memory...\n");
 
 	int i = 0;
-	while (shm [i] != '\0')
+	while (shm [i] != '\0')									//To Convert the read String into Uppercase.
 	{
 		if ((shm [i] >= 'a') && (shm [i] <= 'z'))
 		{
@@ -45,13 +55,13 @@ int main ()
 		i += 1;
 	}
 
-	sleep (10);
+	sleep (5);												//To Wait for the Process-2 to read and reverse the written String on the SHM.
 
 	printf ("Reading from the Shared Memory: %s\n", shm);
 
-	shmdt (shm);
+	shmdt (shm);											//To Detach the Process-1 from the Shared Memory.
 
-	shmctl (shm_id, IPC_RMID, NULL);
+	shmctl (shm_id, IPC_RMID, NULL);						//To Remove the Shared Memory segment.
 
 	return 0;
 }
--- 12_IPC_SHM/Process-2.c
+++ 12_IPC_SHM/Process-2.c
@@ -1,9 +1,19 @@
 /*
 Name         : Prabhat Kiran
-Date         :
-Description  :
-Sample Input :
-Sample Output:
+Date         : 23rd December 2022
+Description  : Implement Communication between TWO different Processes using Shared Memory (SHM).
+1. Create 2 different Process by executing 2 Different a.out (executable) Files.
+2. Create SHM from any one of the Process; either Process-1 or Process-2.
+3. In Process-1:
+a) Prompt the user to enter a String.
+b) Convert the String into Upper case.
+c) Write the Upper case String in Shared Memory.
+4. In Process-2:
+a) Read the Data from the Shared Memory.
+b) Reverse the Input which is read from SHM.
+c) Write the Reverse String in Shared Memory.
+5. Read the Data from Shared memory in Process-1.
+6. Print the Output string on 'stdout'.
 */
 
 #include <stdio.h>
@@ -16,17 +26,17 @@ Sample Output:
 
 int main ()
 {
-	sleep (5);
+	sleep (5);												//To wait for the Process-1 to write the Uppercase String in the SHM.
 
-	int shm_id = shmget ('P', 100, 0);
-	if (shm_id == -1)
+	int shm_id = shmget ('P', 100, 0);						//To Get the SHM ID for the Shared Memory created in Process-1.
+	if (shm_id == -1)										//Error Handling.
 	{
 		perror ("shmget");
 		return 0;
 	}
 
-	char* shm = (char*) shmat (shm_id, NULL, 0);
-	if (shm == (char*) (-1))
+	char* shm = (char*) shmat (shm_id, NULL, 0);			//To Attach the Process-2 with the Shared Memory.
+	if (shm == (char*) (-1))								//Error Handling.
 	{
 		perror ("shmat");
 		return 0;
@@ -35,18 +45,18 @@ int main ()
 	printf ("Reading from the Shared Memory: %s\n", shm);
 
 	int len = 0;
-	while (shm [++len] != '\0');
+	while (shm [++len] != '\0');							//To find the Length of the String read from the SHM.
 
 	printf ("Writing to the Shared Memory...\n");
 
-	for (int i = 0; i <= (len / 2); i++)
+	for (int i = 0; i <= (len / 2); i++)					//To Reverse the String read from the SHM.
 	{
 		char temp = shm [i];
 		shm [i] = shm [len - i - 1];
 		shm [len - i - 1] = temp;
 	}
 
-	shmdt (shm);
+	shmdt (shm);											//To Detach the Process-2 from the Shared Memory.
 
 	return 0;
 }
